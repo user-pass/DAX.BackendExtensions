838cf6381d27890a582b344b998927113db4ddea
Administrator <Administrator@AX365_SLA>
2020-07-15 20:25:36 +0300

    Discount (updated)

--- Extensions.CRTExtensions/Services/SaveCartHandler.cs
+++ Extensions.CRTExtensions/Services/SaveCartHandler.cs
@@ -1,20 +1,47 @@
-﻿//namespace DAX.Runtime.Extensions.CRTExtensions.Services
-//{
-//    using Microsoft.Dynamics.Commerce.Runtime;
-//    using Microsoft.Dynamics.Commerce.Runtime.Messages;
-//    using Microsoft.Dynamics.Commerce.Runtime.Workflow;
+﻿namespace DAX.Runtime.Extensions.CRTExtensions.Services
+{
+    using Microsoft.Dynamics.Commerce.Runtime;
+    using Microsoft.Dynamics.Commerce.Runtime.DataModel;
+    using Microsoft.Dynamics.Commerce.Runtime.Messages;
+    using Microsoft.Dynamics.Commerce.Runtime.Workflow;
+    using System.Linq;
 
-//    public sealed class SaveCartHandler : SingleRequestHandler<SaveCartRequest, SaveCartResponse>
-//    {
-//        protected override SaveCartResponse Process(SaveCartRequest request)
-//        {
-//            ThrowIf.Null(request, "request");
+    public sealed class SaveCartHandler : SingleRequestHandler<SaveCartRequest, SaveCartResponse>
+    {
+        protected override SaveCartResponse Process(SaveCartRequest request)
+        {
+            ThrowIf.Null(request, "request");
 
-//            request.Cart.Comment = "test";
-//            request.Cart.InvoiceComment = "test";
+            foreach (var line in request.Cart.CartLines.ToList())
+            {
+                line.Comment = "test discount";
+            }
 
-//            var requestHandler = new SaveCartRequestHandler();
-//            return request.RequestContext.Runtime.Execute<SaveCartResponse>(request, request.RequestContext, requestHandler, skipRequestTriggers: false);
-//        }
-//    }
-//}
+            if (request.Cart.NetPrice >= 50.0M)
+            {
+
+                request.Cart.TotalManualDiscountPercentage = 10;
+
+                
+
+                //request.Cart.NetPrice = request.Cart.NetPrice - request.Cart.NetPrice / 10;
+
+                //foreach (var line in request.Cart.CartLines.ToList())
+                //{
+                //    line.LineDiscount = line.Price / 10;
+                //    line.DiscountAmount = line.Price / 10;
+                //    line.DiscountAmountWithoutTax = line.Price / 10; ;
+                //    DiscountLine discountLine = new DiscountLine();
+                //    discountLine.Percentage = 10;
+                //    discountLine.Amount = line.Price / 10;
+                //    line.DiscountLines.Add(discountLine);
+                //    line.Price = line.Price - line.Price / 10;
+                //}
+            }
+
+
+            var requestHandler = new SaveCartRequestHandler();
+            return request.RequestContext.Runtime.Execute<SaveCartResponse>(request, request.RequestContext, requestHandler, skipRequestTriggers: true);
+        }
+    }
+}
--- Extensions.CRTExtensions/Services/SaveCartTrigger.cs
+++ Extensions.CRTExtensions/Services/SaveCartTrigger.cs
@@ -1,87 +1,93 @@
-﻿namespace DAX.Runtime.Extensions.CRTExtensions.Services
-{
-    using Microsoft.Dynamics.Commerce.Runtime;
-    using Microsoft.Dynamics.Commerce.Runtime.DataModel;
-    using Microsoft.Dynamics.Commerce.Runtime.Messages;
-    using Microsoft.Dynamics.Commerce.Runtime.Services.Messages;
-    using Microsoft.Dynamics.Commerce.Runtime.Workflow;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
+﻿//namespace DAX.Runtime.Extensions.CRTExtensions.Services
+//{
+//    using Microsoft.Dynamics.Commerce.Runtime;
+//    using Microsoft.Dynamics.Commerce.Runtime.DataModel;
+//    using Microsoft.Dynamics.Commerce.Runtime.Messages;
+//    using Microsoft.Dynamics.Commerce.Runtime.Services.Messages;
+//    using Microsoft.Dynamics.Commerce.Runtime.Workflow;
+//    using System;
+//    using System.Collections.Generic;
+//    using System.Diagnostics;
+//    using System.Linq;
 
-    public class SaveCartTrigger : IRequestTrigger
-    {
-        public IEnumerable<Type> SupportedRequestTypes
-        {
-            get
-            {
-                return new[]
-                {
-                        typeof(SaveCartRequest)
-                };
-            }
-        }
+//    public class SaveCartTrigger : IRequestTrigger
+//    {
+//        public IEnumerable<Type> SupportedRequestTypes
+//        {
+//            get
+//            {
+//                return new[]
+//                {
+//                        typeof(SaveCartRequest)
+//                };
+//            }
+//        }
 
-        public void OnExecuting(Request request)
-        {
+//        public void OnExecuting(Request request)
+//        {
 
-            if (request == null)
-            {
-                throw new ArgumentNullException("request");
-            }
+//            if (request == null)
+//            {
+//                throw new ArgumentNullException("request");
+//            }
 
-            Type requestedType = request.GetType();
+//            Type requestedType = request.GetType();
 
-            if (requestedType == typeof(SaveCartRequest))
-            {
-                this.GetCommentForCart((SaveCartRequest)request);
-            }
-        }
+//            if (requestedType == typeof(SaveCartRequest))
+//            {
+//                this.GetCommentForCart((SaveCartRequest)request);
+//            }
+//        }
 
-        private SaveCartResponse GetCommentForCart(SaveCartRequest request)
-        {
-            
+//        private SaveCartResponse GetCommentForCart(SaveCartRequest request)
+//        {
 
-            if (request.Cart.CartLines.Count > 0)
-            {
-                int sum = 0;
 
-                foreach (var line in request.Cart.CartLines.ToList())
-                {
-                    
-                    sum += Convert.ToInt32(line.Price);
+//            if (request.Cart.CartLines.Count > 0)
+//            {
+//                //decimal sum = 0.0M;
 
-                    CartLine cartLine = new CartLine();
-                    cartLine = line;
-                    cartLine.Comment = "discount";
-                    //DiscountLine discountLine = new DiscountLine();
-                    //discountLine.Percentage = 50;
-                    //cartLine.DiscountLines.Add(discountLine);
-                    request.Cart.CartLines.Remove(line);
-                    request.Cart.CartLines.Add(cartLine);
-                }
+//                foreach (var line in request.Cart.CartLines.ToList())
+//                {
 
-                if (sum >= 50)
-                {
-                    request.Cart.TotalManualDiscountPercentage = 50;
-                }
-            } else
-            {
-                var emptyRequestHandler = new SaveCartRequestHandler();
-                var saveEmptyCartRequest = new SaveCartRequest(request.Cart);
-                return request.RequestContext.Runtime.Execute<SaveCartResponse>(saveEmptyCartRequest, saveEmptyCartRequest.RequestContext, emptyRequestHandler, skipRequestTriggers: true);
-            }
+//                    //sum += line.Price;
 
-            
+//                    CartLine cartLine = new CartLine();
+//                    cartLine = line;
+//                    cartLine.Comment = "discount";
 
-            var requestHandler = new SaveCartRequestHandler();
-            var saveCartRequest = new SaveCartRequest(request.Cart);
-            return request.RequestContext.Runtime.Execute<SaveCartResponse>(saveCartRequest, saveCartRequest.RequestContext, requestHandler, skipRequestTriggers: true);
-        }
+//                    //DiscountLine discountLine = new DiscountLine();
+//                    //discountLine.Percentage = 50;
+//                    //cartLine.DiscountLines.Remove(cartLine.DiscountLines.FirstOrDefault());
+//                    //cartLine.DiscountLines.Add(discountLine);
 
-        public void OnExecuted(Request request, Response response)
-        {
-            
-        }
-    }
-}
+//                    request.Cart.CartLines.Remove(line);
+//                    request.Cart.CartLines.Add(cartLine);
+//                }
+
+
+
+//                if (request.Cart.NetPrice >= 50.0M)
+//                {
+//                    request.Cart.TotalManualDiscountPercentage = 50;
+//                }
+
+//            }
+
+//            //using (EventLog eventLog = new EventLog("Application"))
+//            //{
+//            //    eventLog.Source = "Application";
+//            //    eventLog.WriteEntry("Final sum is " + request.Cart.NetPrice.ToString(), EventLogEntryType.Warning, 101, 1);
+//            //}
+
+
+//            var requestHandler = new SaveCartRequestHandler();
+//            return request.RequestContext.Runtime.Execute<SaveCartResponse>(request, request.RequestContext, requestHandler, skipRequestTriggers: true);
+//        }
+
+//        public void OnExecuted(Request request, Response response)
+//        {
+
+//        }
+//    }
+//}
